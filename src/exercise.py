dc565b99456ea0ca465c6feddc4496dd0a11b128
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 03:59:54 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #write your code below this line
+    num = int(input('Enter number:'))
+    
+    if num > 70:
+        print('Speeding ticket!')
 
 if __name__ == '__main__':
     main()
